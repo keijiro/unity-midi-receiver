24ee3bb8494332076d28f6602ae6c9e1a9d0081f
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-08-13 10:56:27 +0900

    Refactoring.

--- UnityMIDIReceiver/UnityMIDIReceiver.cpp
+++ UnityMIDIReceiver/UnityMIDIReceiver.cpp
@@ -1,72 +1,101 @@
 #import <CoreMIDI/CoreMIDI.h>
-#include <iterator>
-#include <list>
 #include <queue>
-#include <string>
 
 #pragma mark Private classes
 
 namespace
 {
     // MIDI message structure.
+    // TODO: Use union.
     struct Message
     {
+        MIDIUniqueID source;
         Byte status;
-        Byte data[2];
+        Byte data1;
+        Byte data2;
         
-        Message(Byte status, Byte data1, Byte data2)
-        :   status(status)
+        Message(MIDIUniqueID source, Byte status, Byte data1, Byte data2)
+        :   source(source), status(status), data1(data1), data2(data2)
         {
-            data[0] = data1;
-            data[1] = data2;
         }
         
-        operator UInt32() const
+        operator uint64_t() const
         {
-            return static_cast<UInt32>(status) |
-                (static_cast<UInt32>(data[0]) << 8) |
-                (static_cast<UInt32>(data[1]) << 16);
+            return
+                (static_cast<uint64_t>(source) & 0xffffffffULL) |
+                (static_cast<uint64_t>(status) << 32) |
+                (static_cast<uint64_t>(data1) << 40) |
+                (static_cast<uint64_t>(data2) << 48);
         }
     };
+
+    // MIDI source ID array.
+    MIDIUniqueID sourceIDs[256];
+
+    // Incoming MIDI message queue.
+    std::queue<Message> messageQueue;
+
+    // Core MIDI objects.
+    MIDIClientRef client;
+    MIDIPortRef inputPort;
     
-    // MIDI endpoint class.
-    class Endpoint
-    {
-    public:
-        Endpoint()
-        {
-        }
-        
-        Endpoint(SInt32 id, const std::string name)
-        :   id(id), name(name)
-        {
-        }
-        
-        bool operator == (SInt32 id) const
-        {
-            return this->id == id;
-        }
-        
-        SInt32 id;
-        std::string name;
-        std::queue<Message> messageQueue;
-    };
-    
-    // Endpoint list.
-    std::list<Endpoint> endpoints;
+    // Local functions.
+    void ReconnectAllSources();
 }
 
 #pragma mark Core MIDI callback function
 
 namespace
 {
-    extern "C" void MyReadProc(const MIDIPacketList *pktlist, void *readProcRefCon, void *srcConnRefCon)
+    extern "C" void MyMIDIStateChangedHander(const MIDINotification* message, void* refCon)
     {
-        Endpoint& endpoint = *reinterpret_cast<Endpoint*>(srcConnRefCon);
-        // Queue the all incoming packets.
+        // Only process additions and removals.
+        if (message->messageID != kMIDIMsgObjectAdded && message->messageID != kMIDIMsgObjectRemoved) return;
+        
+        // Only process source operations.
+        auto addRemoveDetail = reinterpret_cast<const MIDIObjectAddRemoveNotification*>(message);
+        if (addRemoveDetail->childType != kMIDIObjectType_Source) return;
+        
+        // Update the MIDI client state.
+        ReconnectAllSources();
+    }
+    
+    extern "C" void MyMIDIReadProc(const MIDIPacketList *pktlist, void *readProcRefCon, void *srcConnRefCon)
+    {
+        auto sourceID = *reinterpret_cast<MIDIUniqueID*>(srcConnRefCon);
+        
+        // Transform the packets into MIDI messages and push it to the message queue.
         for (int i = 0; i < pktlist->numPackets; i++) {
-            const MIDIPacket *pkt = &pktlist->packet[i];
-            endpoint.messageQueue.push(Message(pkt->data[0], pkt->data[1], pkt->data[2]));
+            const MIDIPacket& packet = pktlist->packet[i];
+            messageQueue.push(Message(sourceID, packet.data[0], packet.data[1], packet.data[2]));
+        }
+    }
+}
+
+#pragma mark Private functions
+    
+namespace
+{
+    void ReconnectAllSources()
+    {
+        // Dispose the old MIDI client if exists.
+        if (client != nullptr) MIDIClientDispose(client);
+        
+        // Create a MIDI client.
+        MIDIClientCreate(CFSTR("UnityMIDIReceiver Client"), MyMIDIStateChangedHander, nullptr, &client);
+        
+        // Create a MIDI port which covers all MIDI sources.
+        MIDIInputPortCreate(client, CFSTR("UnityMIDIReceiver Input Port"), MyMIDIReadProc, nullptr, &inputPort);
+        
+        // Enumerate the all MIDI sources.
+        ItemCount sourceCount = MIDIGetNumberOfSources();
+        assert(sourceCount < sizeof(sourceIDs));
+        
+        for (int i = 0; i < sourceCount; i++) {
+            // Connect the MIDI source to the input port.
+            MIDIEndpointRef source = MIDIGetSource(i);
+            MIDIObjectGetIntegerProperty(source, kMIDIPropertyUniqueID, &sourceIDs[i]);
+            MIDIPortConnectSource(inputPort, source, &sourceIDs[i]);
         }
     }
 }
@@ -76,72 +105,45 @@ namespace
 // Initializes the plug-in.
 extern "C" void UnityMIDIReceiver_Initialize()
 {
-    static bool initialized = false;
-    if (initialized) return;
-    initialized = true;
-    
-    MIDIClientRef client;
-    MIDIPortRef inputPort;
-    
-    // Create a MIDI client.
-    MIDIClientCreate(CFSTR("MIDI Tester Client"), nil, nil, &client);
-    
-    // Create a MIDI port which covers all MIDI sources.
-    ItemCount sourceCount = MIDIGetNumberOfSources();
-    MIDIInputPortCreate(client, CFSTR("MIDI Tester Input Port"), MyReadProc, nil, &inputPort);
-    
-    // Enumerate the all MIDI sources.
-    for (int i = 0; i < sourceCount; i++) {
-        MIDIEndpointRef source = MIDIGetSource(i);
-        
-        // Retrieve the unique ID of this MIDI source.
-        SInt32 uniqueID;
-        MIDIObjectGetIntegerProperty(source, kMIDIPropertyUniqueID, &uniqueID);
-
-        // Retrieve the redable name of this MIDI source.
-        char modelName[256];
-        CFStringRef modelStringRef;
-        MIDIObjectGetStringProperty(source, kMIDIPropertyModel, &modelStringRef);
-        CFStringGetCString(modelStringRef, modelName, sizeof(modelName), kCFStringEncodingUTF8);
-
-        // Add this MIDI source to the endpoint list.
-        endpoints.push_back(Endpoint(uniqueID, modelName));
-        
-        // Connect the MIDI source to the input port.
-        MIDIPortConnectSource(inputPort, source, &endpoints.back());
-    }
+    ReconnectAllSources();
 }
 
 // Counts the number of endpoints.
 extern "C" int UnityMIDIReceiver_CountEndpoints()
 {
-    return endpoints.size();
+    return MIDIGetNumberOfSources();
 }
 
-// Get the unique ID of an endpoint, with an index on the endpoint list.
-extern "C" unsigned int UnityMIDIReceiver_GetEndpointIDAtIndex(int index)
+// Get the unique ID of an endpoint.
+extern "C" uint32_t UnityMIDIReceiver_GetEndpointIDAtIndex(int index)
 {
-    auto it = endpoints.begin();
-    std::advance(it, index);
-    return it->id;
+    return sourceIDs[index];
 }
 
 // Get the name of an endpoint.
-extern "C" const char* UnityMIDIReceiver_GetEndpointName(unsigned int id)
+extern "C" const char* UnityMIDIReceiver_GetEndpointName(uint32_t id)
 {
-    auto it = std::find(endpoints.begin(), endpoints.end(), static_cast<SInt32>(id));
-    return (it != endpoints.end()) ? it->name.c_str() : nullptr;
+    MIDIObjectRef object;
+    MIDIObjectType type;
+    MIDIObjectFindByUniqueID(id, &object, &type);
+    assert(type == kMIDIObjectType_Source);
+    
+    CFStringRef name;
+    MIDIObjectGetStringProperty(object, kMIDIPropertyDisplayName, &name);
+    
+    static char buffer[256];
+    CFStringGetCString(name, buffer, sizeof(buffer), kCFStringEncodingUTF8);
+    
+    return buffer;
 }
 
-// Retrieve and erase an MIDI message data from an endpoint message queue.
-extern "C" unsigned int UnityMIDIReceiver_DequeueMessageDataOnEndpoint(unsigned int id)
+// Retrieve and erase an MIDI message data from the message queue.
+extern "C" uint64_t UnityMIDIReceiver_DequeueIncomingData()
 {
-    auto it = std::find(endpoints.begin(), endpoints.end(), static_cast<SInt32>(id));
-    if (it == endpoints.end()) return 0;
-    if (it->messageQueue.empty()) return 0;
+    if (messageQueue.empty()) return 0;
 
-    Message m = it->messageQueue.back();
-    it->messageQueue.pop();
+    Message m = messageQueue.back();
+    messageQueue.pop();
     
-    return static_cast<UInt32>(m);
+    return static_cast<uint64_t>(m);
 }
